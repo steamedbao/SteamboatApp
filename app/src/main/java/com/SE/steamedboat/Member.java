5af3be8b8d8277e14dec7045ccfdedf3c95903d5
Tyuanyuan <190060010@qq.com>
2020-03-09 15:50:32 +0800

    ty

--- app/src/main/java/com/SE/steamedboat/Member.java
+++ app/src/main/java/com/SE/steamedboat/Member.java
@@ -2,6 +2,10 @@ package com.SE.steamedboat;
 
 public class Member {
     private String memberName;
+    private int memberID;
+    private float amountPaid;
+    private float amountIncurred;
+    private boolean host;
 
     public Member (String name) {
         setMemberName(name);
@@ -13,4 +17,24 @@ public class Member {
     }
 
     public String getMemberName(){return memberName;}
+
+    public int getMemberID() {return memberID;}
+
+    public void setMemberID(int memberID) {this.memberID = memberID;}
+
+    public float getAmountPaid() {return amountPaid;}
+
+    protected void setAmountPaid(float amountPaid) {this.amountPaid = amountPaid;}
+
+    public float getAmountIncurred() {return amountIncurred;}
+
+    protected void setAmountIncurred(float amountIncurred) {this.amountIncurred = amountIncurred;}
+
+    public boolean isHost() {return host;}
+
+    public void setHost(boolean host) {this.host = host;}
+
+    public void removeMember(){} //need edit!!!!!!!!!!!!!!!
+
+    public boolean verifyMember(){return true;} //need edit!!!!!!!!!!!!!!!
 }
