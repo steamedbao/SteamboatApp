e4eaac7de91f27505fefed85d6f81ef5883136fe
Tyuanyuan <190060010@qq.com>
2020-03-12 21:53:58 +0800

    ty

--- app/src/main/java/com/SE/steamedboat/Transaction.java
+++ app/src/main/java/com/SE/steamedboat/Transaction.java
@@ -1,4 +1,55 @@
 package com.SE.steamedboat;
 
+import java.util.ArrayList;
+
 public class Transaction {
+    private String transactionCurrency;
+    private float transactionAmount;
+    private int payer;
+    private float homeWorth;
+    private float exchangeRate;
+
+    public Transaction(float transactionAmount, int payer, float exchangeRate) {
+        this.transactionAmount = transactionAmount;
+        this.payer = payer;
+        this.exchangeRate = exchangeRate;
+    }
+    public void findHomeWorth(){}
+    public float sumAmountPaid(){
+        return transactionAmount;
+    }
+    public ArrayList<Float> sumAmountIncurred(){
+        ArrayList<Float> amountIncurred = new ArrayList<Float>();
+        return amountIncurred;
+    }
+    public String getTransactionCurrency() {
+        return transactionCurrency;
+    }
+    public void setTransactionCurrency(String transactionCurrency) {
+        this.transactionCurrency = transactionCurrency;
+    }
+    public float getTransactionAmount() {
+        return transactionAmount;
+    }
+    public void setTransactionAmount(float transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+    public int getPayer() {
+        return payer;
+    }
+    public void setPayer(int payer) {
+        this.payer = payer;
+    }
+    public float getHomeWorth() {
+        return homeWorth;
+    }
+    public void setHomeWorth(float homeWorth) {
+        this.homeWorth = homeWorth;
+    }
+    public float getExchangeRate() {
+        return exchangeRate;
+    }
+    public void setExchangeRate(float exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
 }
